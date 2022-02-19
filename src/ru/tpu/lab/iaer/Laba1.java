19650bace54bb451a0221858235f5ba04554f07a
dima <dima@tpu.ru>
2022-02-19 12:58:05 +0700

    Initial commit

---
+++ src/ru/tpu/lab/iaer/Laba1.java
@@ -0,0 +1,11 @@
+package ru.tpu.lab.iaer;/*
+ * @author timtims
+ * @date 19.02.2022
+ */
+
+public class Laba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
